39ad3acdd7b1056096db504818a9a7ec593b846a
Calvin <calvinchan87@gmail.com>
2021-04-10 19:19:14 +0000

    fetchCoordsByIP request with Promises

--- index2.js
+++ index2.js
@@ -1,6 +1,12 @@
 const { fetchMyIP } = require('./iss_promised');
+const { fetchCoordsByIP } = require('./iss_promised');
+
+// fetchMyIP()
+//   .then((body) => {
+//     console.log(body);
+// });
 
 fetchMyIP()
-  .then((body) => {
-    console.log(body);
-});
+  // .then(body => fetchCoordsByIP(body))
+  .then(fetchCoordsByIP)
+  .then(body => console.log(body));
